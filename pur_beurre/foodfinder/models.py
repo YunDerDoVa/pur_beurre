14c62701f10325bf48ec1f08ead5a682a3efa653
ArthurNeyer <arthur.neyer@protonmail.com>
2020-06-01 21:02:13 +0200

    write tests, edit models

--- pur_beurre/foodfinder/models.py
+++ pur_beurre/foodfinder/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
@@ -9,3 +10,19 @@ class SearchFoodRequests(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
     search = models.CharField(max_length=255)
     #food = models.ForeignKey(Food, on_delete=models.SET_NULL, null=True)
+
+class SearchAlgorythm(models.Model):
+
+    class AlgorythmMChoice(models.TextChoices):
+
+        BY_FAT = 'ByFat', _('By Fat')
+        BY_SALT = 'BySalt', _('By Salt')
+        BY_NUTRIMENTS = 'ByNutriments', _('By Nutriments')
+        BY_CATEGORY = 'ByCategory', _('By Category')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    algorythm = models.CharField(
+        max_length=63,
+        choices=AlgorythmMChoice.choices,
+        default=AlgorythmMChoice.BY_FAT,
+    )
---
+++ pur_beurre/mugauth/tests/test_models.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from django.conf import settings
+from django.contrib.auth import login, authenticate, logout
+
+from mugauth.models import Account
+
+# Create your tests here.
+class AccountTestCase(TestCase):
+
+    USERNAME = 'TestUser'
+    EMAIL = 'test@email.com'
+    PASSWORD = 'password'
+
+    def setUp(self):
+        Account.objects.create_user(username=self.USERNAME, email='test@email.com', password='password')
+
+    def test_auth_user_model(self):
+        model = settings.AUTH_USER_MODEL
+        self.assertEqual(model, 'mugauth.Account')
+
+    def test_authenticate(self):
+        account = authenticate(username=self.USERNAME, password=self.PASSWORD)
+        self.assertEqual(account.username, self.USERNAME)
