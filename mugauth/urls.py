b9ce1cc253450f8f83c34888dd1335b067a067b3
ArthurNeyer <arthur.neyer@protonmail.com>
2020-07-02 16:02:15 +0200

    fix urls

--- mugauth/urls.py
+++ mugauth/urls.py
@@ -8,6 +8,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register_view, name='register_view'),
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html.django'), name='login_view'),
+    path('login/', views.login_view, name='login_view'),
+    #path('login/', auth_views.LoginView.as_view(template_name='auth/login.html.django'), name='login_view'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout_view'),
 ]
