e2e52964b1365dd22f0ec4d84557b56a8eef5727
ArthurNeyer <arthur.neyer@protonmail.com>
2020-07-23 22:13:45 +0200

    Fix bugs finish

---
+++ mugauth/tests/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+from django.urls import reverse
+
+
+from mugauth.models import Account
+
+# Create your tests here.
+class MugauthViewsTestCase(TestCase):
+
+    USERNAME = 'TestUser'
+    EMAIL = 'test@email.com'
+    PASSWORD = 'password'
+
+    def setUp(self):
+        Account.objects.create_user(username=self.USERNAME, email=self.EMAIL, password=self.PASSWORD)
+
+    def test_next_redirection(self):
+
+        url = reverse('login_view') + '?next=' + reverse('legacy')
+        data = {
+            'email': self.EMAIL,
+            'password': self.PASSWORD,
+        }
+
+        response = self.client.post(url, data, follow=False)
+
+        self.assertTemplateUsed(response, 'foodfinder/legacy.html.django')
