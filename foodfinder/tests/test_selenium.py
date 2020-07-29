e25a5fd7a0991a7eba4e0555b7519d05bdc7b9a2
ArthurNeyer <arthur.neyer@protonmail.com>
2020-07-29 17:30:56 +0200

    Fix tests

--- foodfinder/tests/test_selenium.py
+++ foodfinder/tests/test_selenium.py
@@ -15,38 +15,29 @@ class FoodfinderSeleniumTestCase(LiveServerTestCase):
     PASSWORD = 'password'
 
     def setUp(self):
-        try:
-            self.selenium = webdriver.Firefox()
-            super(FoodfinderSeleniumTestCase, self).setUp()
-        except:
-            pass
-
+        self.selenium = webdriver.Firefox()
+        super(FoodfinderSeleniumTestCase, self).setUp()
 
     def tearDown(self):
-        try:
-            self.selenium.quit()
-            super(FoodfinderSeleniumTestCase, self).tearDown()
-        except:
-            pass
-
+        self.selenium.quit()
+        super(FoodfinderSeleniumTestCase, self).tearDown()
 
     def test_save_button(self):
-        if os.environ.get('TRAVIS', 'False') != 'False':
-            selenium = self.selenium
-            #Opening the link we want to test
-            selenium.get('http://127.0.0.1:8000/')
-            #find the form element
-            email = selenium.find_element_by_id('food_input')
+        selenium = self.selenium
+        #Opening the link we want to test
+        selenium.get('http://127.0.0.1:8000/')
+        #find the form element
+        email = selenium.find_element_by_id('food_input')
 
-            submit = selenium.find_element_by_name('submit')
+        submit = selenium.find_element_by_name('submit')
 
-            #Fill the form with data
-            email.send_keys('tomates')
+        #Fill the form with data
+        email.send_keys('tomates')
 
-            #submitting the form
-            submit.send_keys(Keys.RETURN)
+        #submitting the form
+        submit.send_keys(Keys.RETURN)
 
-            #check the returned result
-            test_1 = 'sauvegarder' in selenium.page_source
-            self.assertTrue(not test_1)
-            self.assertTemplateUsed('foodfinder/results_page.html.django')
+        #check the returned result
+        test_1 = 'sauvegarder' in selenium.page_source
+        self.assertTrue(not test_1)
+        self.assertTemplateUsed('foodfinder/results_page.html.django')
--- mugauth/tests/test_selenium.py
+++ mugauth/tests/test_selenium.py
@@ -14,38 +14,29 @@ class AuthSeleniumTestCase(LiveServerTestCase):
     PASSWORD = 'password'
 
     def setUp(self):
-        try:
-            self.selenium = webdriver.Firefox()
-            super(FoodfinderSeleniumTestCase, self).setUp()
-        except:
-            pass
-
+        self.selenium = webdriver.Firefox()
+        super(AuthSeleniumTestCase, self).setUp()
 
     def tearDown(self):
-        try:
-            self.selenium.quit()
-            super(FoodfinderSeleniumTestCase, self).tearDown()
-        except:
-            pass
-
+        self.selenium.quit()
+        super(AuthSeleniumTestCase, self).tearDown()
 
     def test_next_redirect(self):
-        if os.environ.get('TRAVIS', 'False') != 'False':
-            selenium = self.selenium
-            #Opening the link we want to test
-            selenium.get('http://127.0.0.1:8000/favor/')
-            #find the form element
-            email = selenium.find_element_by_id('id_email')
-            password = selenium.find_element_by_id('id_password')
+        selenium = self.selenium
+        #Opening the link we want to test
+        selenium.get('http://127.0.0.1:8000/favor/')
+        #find the form element
+        email = selenium.find_element_by_id('id_email')
+        password = selenium.find_element_by_id('id_password')
 
-            submit = selenium.find_element_by_name('submit')
+        submit = selenium.find_element_by_name('submit')
 
-            #Fill the form with data
-            email.send_keys(self.EMAIL)
-            password.send_keys('self.PASSWORD')
+        #Fill the form with data
+        email.send_keys(self.EMAIL)
+        password.send_keys('self.PASSWORD')
 
-            #submitting the form
-            submit.send_keys(Keys.RETURN)
+        #submitting the form
+        submit.send_keys(Keys.RETURN)
 
-            #check the returned result
-            self.assertTemplateUsed('foodfinder/favors_page.html.django')
+        #check the returned result
+        self.assertTemplateUsed('foodfinder/favors_page.html.django')
